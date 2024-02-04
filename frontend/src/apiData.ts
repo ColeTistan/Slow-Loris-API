7b27bcf674069d1b5a9bccfa6534a7cfa27bcb22
coletistan <coletistan@gmail.com>
2024-02-03 19:53:07 -0500

    Removed old client directory using frontend directory.

---
+++ frontend/src/apiData.ts
@@ -0,0 +1,34 @@
+const apiData = [
+  {
+    id: 1,
+    title: "All slow loris data",
+    url: "https://slow-loris-api.onrender.com/api/all",
+  },
+  {
+    id: 2,
+    title: "Slow loris by ID",
+    url: "https://slow-loris-api.onrender.com/api/61ff53ccf22e0e457c704641",
+  },
+  {
+    id: 3,
+    title: "Slow loris by scientific name",
+    url: "https://slow-loris-api.onrender.com/api/scientific/Nycticebus coucang",
+  },
+  {
+    id: 4,
+    title: "Slow loris by common name",
+    url: "https://slow-loris-api.onrender.com/api/common/javan",
+  },
+  {
+    id: 5,
+    title: "Slow lorises by native habitat",
+    url: "https://slow-loris-api.onrender.com/api/habitat/laos",
+  },
+  {
+    id: 6,
+    title: "Slow loris by conservation status",
+    url: "https://slow-loris-api.onrender.com/api/status/vulnerable",
+  },
+];
+
+export default apiData;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,34 +1,29 @@
 import "./style.css";
+import apiData from "./apiData";
 
-const randomImg = document.getElementById("random-image-box");
-console.log(randomImg);
+const documentationGrid = document.getElementById("documentation-grid");
 
-const getSlowLorisData = () => {
-  fetch("https://slow-loris-api.onrender.com/api/all")
-    .then((response) => response.json())
-    .then((data) => {
-      const randomData = data[Math.floor(Math.random() * data.length)];
-      randomImg!.innerHTML = `<img class="w-60 h-auto" src="${randomData.image_url}" alt="random image" />`;
-      console.log(randomData);
-    });
+const createApiCard = (heading: string, apiRoute: string) => {
+  // create html elements
+  let card = document.createElement("div");
+  let cardTitle = document.createElement("p");
+  let cardLink = document.createElement("a");
+
+  // parse data into html elements created and set attributes
+  cardTitle.innerText = `GET - ${heading}`;
+  cardLink.innerText = apiRoute;
+  cardLink.href = apiRoute;
+
+  // add css classes to style HTML elements
+  card.classList.add("card");
+  cardTitle.classList.add("card-title");
+  cardLink.classList.add("card-link");
+
+  card.appendChild(cardTitle);
+  card.appendChild(cardLink);
+  documentationGrid?.appendChild(card);
 };
 
-getSlowLorisData();
-
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+apiData.forEach((data) => {
+  createApiCard(data.title, data.url);
+})
